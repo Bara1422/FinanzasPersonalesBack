b5344323e6d71d73018bac1ba69c10ea60bc3fd2
bara1422 <xxbaraxx@hotmail.com>
2025-11-11 17:15:49 -0300

    add: customError en categorias y notificacion

--- src/controllers/categorias.controller.ts
+++ src/controllers/categorias.controller.ts
@@ -1,32 +1,38 @@
 import type { TipoCategoria } from "@prisma/client";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import type { CategoryService } from "../services/category.service";
+import { CustomError } from "../utils/CustomError";
 
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
-  getAllCategories = async (_req: Request, res: Response) => {
+  getAllCategories = async (
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const categories = await this.categoryService.getAllCategories();
       return res.json(categories);
     } catch (error) {
-      return res.status(500).json({
-        message: "Error al obtener las categorías",
-        error: error.message || error,
-      });
+      next(error);
     }
   };
 
-  getCategoriesByType = async (req: Request, res: Response) => {
+  getCategoriesByType = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
-      const tipo = req.params.tipo as TipoCategoria;
+      const tipo = req.params.tipo.toUpperCase() as TipoCategoria;
+      if (!tipo || (tipo !== "INGRESO" && tipo !== "GASTO")) {
+        throw new CustomError("Tipo de categoría inválido", 400);
+      }
       const categories = await this.categoryService.getCategory(tipo);
       return res.json(categories);
     } catch (error) {
-      return res.status(500).json({
-        message: "Error al obtener las categorías por tipo",
-        error: error.message || error,
-      });
+      next(error);
     }
   };
 }
